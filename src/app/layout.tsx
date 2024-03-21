282c410b2c4eb9915801fa02c24f548f2cf0f5c9
paulalextech <psteane123@hotmail.com>
2024-03-21 16:30:47 +0000

    adjust png to ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import { ThemeProvider } from 'next-themes';
+import Head from './head';
 
 export default function RootLayout({
 	children,
